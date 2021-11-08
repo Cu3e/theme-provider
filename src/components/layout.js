f9033816943b7cd263256b5b2b801f641c01a13c
Mateusz Bodzioch <bodzioch.mt@gmail.com>
2021-11-09 00:06:42 +0100

    initial commit

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,55 +1,34 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.com/docs/use-static-query/
- */
+import React from "react"
+import { createTheme, ThemeProvider } from "@material-ui/core/styles"
 
-import * as React from "react"
-import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
+const theme = createTheme({
+  typography: {
+    fontSize: 25,
+    fontFamily: ["Poppins", "sans-serif"].join(", "),
+    body1: {
+      fontSize: 25,
+    },
+    body2: {
+      fontSize: 20,
+    },
+  },
+  palette: {
+    primary: {
+      main: "#ffc0cb",
+    },
+    secondary: {
+      main: "#ffc0cb",
+    },
+  },
+})
 
-import Header from "./header"
-import "./layout.css"
-
-const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
-
+const Layout = (props) => {
   return (
-    <>
-      <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
-        }}
-      >
-        <main>{children}</main>
-        <footer
-          style={{
-            marginTop: `2rem`,
-          }}
-        >
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.com">Gatsby</a>
-        </footer>
-      </div>
-    </>
+    <ThemeProvider theme={theme}>
+      <div style={{ color: "green" }}>theme.palette.primary.main: {theme.palette.primary.main} created in layout.js via createTheme</div>
+      <main>{props.children}</main>
+    </ThemeProvider>
   )
-}
-
-Layout.propTypes = {
-  children: PropTypes.node.isRequired,
 }
 
 export default Layout
